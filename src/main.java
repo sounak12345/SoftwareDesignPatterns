a5583d41e3759a7baf7da4b6e49a7678dba8a9f9
SounakMukherjee <sounak.mukherjee1234@gamil.com>
2024-09-19 20:04:13 -0700

    Added main implement observer

--- src/main.java
+++ src/main.java
@@ -1,7 +1,15 @@
-public class WeatherApp {
+
+public class WeatherStation {
     public static void main(String[] args) {
+        WeatherData appData = new WeatherData();
+        CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(appData);
+        WeatherStatisticsDisplay statisicsDisplay = new WeatherStatisticsDisplay(appData);
+        WeatherThirdPartyDisplay thirdPartyDisplay = new WeatherThirdPartyDisplay(appData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(appData);
 
-
+        appData.setMeasurements(80,72,30.4f);
+        appData.setMeasurements(82,70,29.2f);
+        appData.setMeasurements(78,90,29.2f);
 
     }
 }
